9fa8af7e612b2895e68f6b572bd4c2a1af253879
Warattaya09 <pangpang.23wp@gmail.com>
2025-07-30 20:48:09 +0700

    add course

--- backend/entity/Announcement.go
+++ backend/entity/Announcement.go
@@ -7,10 +7,10 @@ import (
 )
 type Announcement struct {
 	gorm.Model
-	Announcement_Title  string
-	Announcement_Date time.Time
-	Announcement_Time   time.Time
-	Announcement_Text string
-	Announcement_Status string
+	Announcement_Title  	string
+	Announcement_Date 		time.Time
+	Announcement_Time   	time.Time
+	Announcement_Text 		string
+	Announcement_Status 	string
 	
 }
--- backend/entity/Course.go
+++ backend/entity/Course.go
@@ -1,1 +1,15 @@
-//รายวิชา
+// รายวิชา
+package entity
+
+import "gorm.io/gorm"
+
+type Course struct{
+	gorm.Model
+	Course_Code		string 
+	Course_Name		string
+	Credit_Num 		float32
+	Classroom		int
+	Class_in_week 	int
+	Hours_of_term 	int
+
+}
