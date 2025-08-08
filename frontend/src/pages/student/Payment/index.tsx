068070b2250824b063c11c32d2330a74c1aa39a6
Thamonwan Kroenkratok <singtokrist1828@gmail.com>
2025-08-08 21:36:35 +0700

    paymentDel

--- frontend/src/pages/student/Payment/index.tsx
+++ frontend/src/pages/student/Payment/index.tsx
@@ -109,14 +109,4 @@ function index() {
 
 export default index;
 
-const Payment = () => {
-  return (
-    <div>
-      <h1>ชำระเงิน</h1>
-
-      {/* Add your schedule content here */}
-    </div>
-  );
-};
-export default Payment;
 
