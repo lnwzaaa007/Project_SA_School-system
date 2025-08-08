3cbf10b9846939360a7d3fe691d195b7e3f39256
mag <theeraphat.j2004@gmail.com>
2025-08-08 21:45:47 +0700

    add...

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
 
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
-import StudentLayout from "./layout/StudentLayout";
-import TeacherLayout from "./layout/TeacherLayout";
-import AdminLayout from "./layout/AdminLayout";
+// import StudentLayout from "./layout/StudentLayout";
+// import TeacherLayout from "./layout/TeacherLayout";
+// import AdminLayout from "./layout/AdminLayout";
 import ConfigRoutes from "./routes";
-import filesubmit from './pages/student/AssignSubmit/FileSubmit'
+// import filesubmit from './pages/student/AssignSubmit/FileSubmit'
 
 function App() {
   return (
